1723186d173189f91a9f22b3c4f27789ad463bf7
Thomas Kay <tomxkay@pm.me>
2023-02-07 00:17:21 -0500

    refactor(plugin): group null-ls sources by filetype

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,26 +2,19 @@ return function(config)
   local null_ls = require "null-ls"
 
   config.sources = {
-    -- Set a formatter
-    -- [TypeScript]
+    -- TypeScript
     null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.prettierd,
-    -- [Lua]
-    null_ls.builtins.formatting.stylua,
-    -- [Ruby]
-    null_ls.builtins.formatting.rubocop,
-
-    -- Set a diagnostic
-    -- [TypeScript]
     null_ls.builtins.diagnostics.eslint_d,
-    -- [Ruby]
-    null_ls.builtins.diagnostics.rubocop,
-    -- [Lua]
+    null_ls.builtins.code_actions.eslint_d,
+
+    -- Lua
+    null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.sumneko_lua,
 
-    -- Set a code action
-    -- [TypeScript]
-    null_ls.builtins.code_actions.eslint_d,
+    -- Ruby
+    null_ls.builtins.formatting.rubocop,
+    null_ls.builtins.diagnostics.rubocop,
   }
 
   return config
