e624d40b40af3ef1c3906b239636f0ee262ff777
Thomas Kay <tomxkay@pm.me>
2023-02-07 00:14:38 -0500

    chore: remove example comments

--- options.lua
+++ options.lua
@@ -1,15 +1,3 @@
--- set vim options here (vim.<first_key>.<second_key> = value)
-
--- If you need more control, you can use the function()...end notation
--- options = function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end,
-
 return {
   opt = {
     -- set to true or false etc.
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,27 +1,3 @@
--- Configure plugins
-
--- You can disable default plugins as follows:
--- ["goolord/alpha-nvim"] = { disable = true },
-
--- You can also add new plugins here as well:
--- Add plugins, the packer syntax without the "use"
--- { "andweeb/presence.nvim" },
--- {
---   "ray-x/lsp_signature.nvim",
---   event = "BufRead",
---   config = function()
---     require("lsp_signature").setup()
---   end,
--- },
-
--- We also support a key value style plugin definition similar to NvChad:
--- ["ray-x/lsp_signature.nvim"] = {
---   event = "BufRead",
---   config = function()
---     require("lsp_signature").setup()
---   end,
--- },
-
 return {
   init = {
     -- Theme
